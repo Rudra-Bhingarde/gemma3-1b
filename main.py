719ff3298105412bae0ac3c1c117601ba5b8bfe4
Rudra-Bhingarde <rudrabhingardedevacc@gmail.com>
2026-04-10 16:58:58 +0530

    added ollama setup instructions

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from langchain_ollama import OllamaLLM
 
 model = OllamaLLM(model='gemma3:1b')
 
-result = model.invoke(input="what food do you eat?")
+result = model.invoke(input="what is your name?")
 print(result)
